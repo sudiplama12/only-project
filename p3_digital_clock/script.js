8103784ab3d2157839009264669bf1ed0314caec
sudiplama12 <lsudip077@gmail.com>
2025-07-16 19:13:56 +0545

    Describe what you code

--- p3_digital_clock/script.js
+++ p3_digital_clock/script.js
@@ -1,7 +1,7 @@
 setInterval(() => {
-    const currenttime = new Date();
+  const currenttime = new Date();
 
-    hrs.innerHTML = String(currenttime.getHours()).padStart(2, '0');
-    min.innerHTML = String(currenttime.getMinutes()).padStart(2, '0');
-    sec.innerHTML = String(currenttime.getSeconds()).padStart(2, '0');
+  document.getElementById("hrs").innerHTML = String(currenttime.getHours()).padStart(2, '0');
+  document.getElementById("min").innerHTML = String(currenttime.getMinutes()).padStart(2, '0');
+  document.getElementById("sec").innerHTML = String(currenttime.getSeconds()).padStart(2, '0');
 }, 1000);
